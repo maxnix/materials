b3ca73a477740f15b79afa2fa6ae9d6620e84eef
Omar De Angelis <omar.hpvfilm@gmail.com>
2024-02-26 12:19:04 +0100

    fix email plugin

--- react/projects/bootcamp/backend/config/plugins.ts
+++ react/projects/bootcamp/backend/config/plugins.ts
@@ -17,28 +17,6 @@ export default ({ env }) => ({
       jwt: {
         expiresIn: "7d",
       },
-      // email: {
-      //   config: {
-      //     provider: "nodemailer",
-      //     providerOptions: {
-      //       host: env("SMTP_HOST"),
-      //       port: env("SMTP_PORT"),
-      //       auth: {
-      //         user: env("SMTP_USERNAME"),
-      //         pass: env("SMTP_PASSWORD"),
-      //       },
-      //       pool: true,
-      //       logger: true,
-      //       debug: true,
-      //       maxConnections: 10000,
-      //     },
-
-      //     settings: {
-      //       defaultFrom: env("DEFAULT_EMAIL"),
-      //       defaultReplyTo: env("DEFAULT_EMAIL"),
-      //     },
-      //   },
-      // },
     },
   },
   // ...
